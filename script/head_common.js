4a459d1ef09985c80497edbb6c8fe8335f166f96
余金堂 <shanquan54@163.com>
2016-08-09 12:05:50 +0800

    add

---
+++ script/head_common.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yujintang on 16/8/9.
+ */
+
+var head_common = '\
+    <title>七日记</title>\
+    <link type="text/css" ; rel="stylesheet" ; href="../css/style.css">\
+    <script src="../jquery-3.1.0.min.js"></script>\
+    \
+   <link rel=”shortcut icon” href=”../favicon.ico”>\
+    <link rel=”shortcut” href=”/favicon.ico” />\
+    <link href=”../favicon.ico” rel=”shortcut icon”>\
+    \
+    <meta name="viewport"\
+content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">\
+    <meta name="apple-touch-fullscreen" content="YES">\
+    <meta name="apple-mobile-web-app-capable" content="yes">\
+    <meta name="applicable-device" content="pc,mobile">';
+
+document.write(head_common);
